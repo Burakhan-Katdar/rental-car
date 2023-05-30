932d72ede31c55fccb5dc5cbb7c7b72a38f1b729
Burakhan Katdar <burakhankatdarr@gmail.com>
2023-05-30 14:22:58 +0300

    Gerekli Scss dosyaları oluşturuldu

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import "./scss/main.scss";
+import 'bootstrap/dist/css/bootstrap.css';
+function App() {
+  return (
+    <div className="App">
+      
+      <h1 className="Burakhan">
+        Burakhan
+      </h1>
+      
+    </div>
+  );
+}
+
+export default App;
