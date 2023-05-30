75c64cc40b9996121524a612ebb2c9f3bc306a97
Burakhan Katdar <burakhankatdarr@gmail.com>
2023-05-30 14:04:02 +0300

    Scss ve Bootstrap projeye yüklendi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import Deneme from './Components/deneme'; 
+import Deneme from "./Components/deneme";
 
-import './App.css';
-
+import "./scss/main.scss";
+import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <div className="App">
       <Deneme />
+
+      <div className="container">
+        <div className="row">
+          <div className='col-3'>
+            a
+          </div>
+          <div className='col-7'>
+            b
+          </div>
+          <div className='col-2'>
+            c
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
